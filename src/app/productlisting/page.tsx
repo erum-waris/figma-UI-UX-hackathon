a5cf4605055974b145df2617a0e851dfe4a9e613
erum-waris <erumwaris93@gmail.com>
2025-01-04 00:34:12 +0500

    updated

--- src/app/productlisting/page.tsx
+++ src/app/productlisting/page.tsx
@@ -4,7 +4,7 @@ import ProductListingSection from '../components/product-listing/ProductListingS
 function ProductListing() {
   return (
    
-        <div className='mx-auto md:max-w-[1440px] max-w-[390px]'>
+        <div className='mx-auto'>
        <ProductListingSection/>
         </div>
     
